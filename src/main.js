672200aa285db5b88a248c8018e2f6c6cebb2bea
angieli13 <angiequimica13@gmail.com>
2023-01-04 14:12:51 -0500

    4 de enero 23

--- src/main.js
+++ src/main.js
@@ -4,7 +4,8 @@ import data_harry from "./data.js";
 
 
 
-document.getElementById("btnBooks").addEventListener("click", function(){
+document.getElementById("booksh").addEventListener("click", function(){
+    console.log("sipi")
     const datos_books = data_harry.filterData(data, "books");
     
     let divs_escritura = document.getElementById('books_div');
@@ -15,13 +16,18 @@ document.getElementById("btnBooks").addEventListener("click", function(){
         const valor = datos_books[books];
         console.log({books, valor});
         console.log(valor["title"]);
-        divs_escritura.innerHTML += "<div><h1 style='color:#fff'>" + valor["title"] + "</h1>" +
-                                    "<p style='color:#fff'> " + valor["author"]  + "</p>" +
-                                    "<p style='color:#fff'> " + valor["releaseDay"]  + "</p>"+
-                                    "<p style='color:#fff'> " + valor["description"]  + "</p></div>";
-                                    
+        divs_escritura.innerHTML += "<div class='card'>"+
+                                        "<img src='./images/libropar.png' alt='imgbook' style='width:100px; height: 100px;'>" +
+                                        "<div class='container'>" +
+                                        "<h1 style='color:#fff'>" + valor["title"] + "</h1>" +
+                                        "<h2 style='color:#fff'> " + valor["author"]  + "</h2>" +
+                                        "<p style='color:#fff'> " + valor["releaseDay"]  + "</p>"+
+                                        "<p style='color:#fff'> " + valor["description"]  + "</p>" +
+                                        "</div>" +
+                                    "</div>";                                    
                                    
     }
+    
 
 });
 
