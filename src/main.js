078a050df4be5b6292bd01ea908423ff428cf4e0
= <=>
2023-01-05 19:29:12 -0300

    Cambios estilo cards

--- src/main.js
+++ src/main.js
@@ -5,18 +5,18 @@ import data_harry from "./data.js";
 
 
 document.getElementById("booksh").addEventListener("click", function(){
-    console.log("sipi")
-    const datos_books = data_harry.filterData("books");
+  //console.log("sipi")
+  const datos_books = data_harry.filterData("books");
     
-    let divs_escritura = document.getElementById('books_div');
+  const divs_escritura = document.getElementById('books_div');
 
-    console.log(datos_books)
+  //console.log(datos_books)
 
-    for (let books in datos_books){
-        const valor = datos_books[books];
-        console.log({books, valor});
-        console.log(valor["title"]);
-        divs_escritura.innerHTML += "<div class='card'>"+
+  for (const books in datos_books){
+    const valor = datos_books[books];
+    //console.log({books, valor});
+    //console.log(valor["title"]);
+    divs_escritura.innerHTML += "<div class='card'>"+
                                         "<img src='./images/libropar.png' alt='imgbook' style='width:100px; height: 100px;'>" +
                                         "<div class='container'>" +
                                         "<h1 style='color:#fff'>" + valor["title"] + "</h1>" +
@@ -26,24 +26,24 @@ document.getElementById("booksh").addEventListener("click", function(){
                                         "</div>" +
                                     "</div>";                                    
                                    
-    }
+  }
 
 
-});
+}),
 
 document.getElementById("charactersh").addEventListener("click", function(){
-    console.log("sipi")
-    const datos_characters = data_harry.filterData( "characters");
+  //console.log("sipi")
+  const datos_characters = data_harry.filterData( "characters");
     
-    let divs_personajes = document.getElementById('characters_div');
+  const divs_escritura = document.getElementById('books_div');
 
-    console.log(datos_characters)
+  //console.log(datos_characters)
 
-    for (var characters in datos_characters){
-        const valor = datos_characters[characters];
-        console.log({characters, valor});
-        console.log(valor["name"]);
-        divs_personajes.innerHTML += "<div class='card'>"+
+  for (const characters in datos_characters){
+    const valor = datos_characters[characters];
+    //console.log({characters, valor});
+    //console.log(valor["name"]);
+    divs_escritura.innerHTML += "<div class='card'>"+
                                         "<div class='container'>" +
                                         "<h1 style='color:#fff'>" + valor["name"] + "</h1>" +
                                         "<h2 style='color:#fff'> " + valor["species"]  + "</h2>" +
@@ -51,12 +51,12 @@ document.getElementById("charactersh").addEventListener("click", function(){
                                         "</div>" +
                                     "</div>";                                    
                                    
-    }
-    //20 -> 21 
-    //const i = i + 1;  
-    //const html = "<p>a|bbb</p>"
-   // html += "<a></a>"//
-    // <p></p><a></a>
+  }
+  //20 -> 21 
+  //const i = i + 1;  
+  //const html = "<p>a|bbb</p>"
+  // html += "<a></a>"//
+  // <p></p><a></a>
 });
 
 
